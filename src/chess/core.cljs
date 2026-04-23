8d4c2420783d3fb28519eb417e6caa5aad1bdd12
Christian Stjernberg <christian.stjernberg@omegapoint.se>
2026-04-23 23:03:13 +0200

    Refactor spec validation

--- src/chess/core.cljs
+++ src/chess/core.cljs
@@ -6,24 +6,14 @@
    [chess.rules :as rules]
    [chess.scriptsAndExamples :as scripts]))
 
-(s/def ::ok string?)
-(s/def ::error string?)
 (s/def ::game-history ::d/history)
-(s/def ::move-result (s/or :success (s/keys :req-un [::ok])
-                           :failure (s/keys :req-un [::error])))
 
 (def game-history
   "Vector of move maps in order: {:from [:e :2] :to [:e :4]}."
   (atom [] :validator #(s/valid? ::game-history %)))
 
-(s/fdef get-state
-  :ret ::d/board)
-
 (defn get-state []
   (helpers/get-board-state @game-history))
-
-(s/fdef get-history
-  :ret ::d/annotated-history)
 
 (defn- history-with-check-states [history]
   (mapv (fn [idx move]
@@ -39,15 +29,12 @@
 (defn check? []
   (rules/in-check? @game-history))
 
-(s/fdef play-move
-  :args (s/cat :from ::d/square :to ::d/square :promotion (s/? ::d/promotion))
-  :ret  ::move-result)
-
 (defn play-move
   ([from to] (play-move from to nil))
   ([from to promotion]
    (s/assert ::d/square from)
    (s/assert ::d/square to)
+   (when promotion (s/assert ::d/promotion promotion))
    (if (rules/legal-move? @game-history from to promotion)
      (do
        (swap! game-history conj (cond-> {:from from :to to}
@@ -58,4 +45,6 @@
 (defn pawn-reaching-last-rank?
   "Returns true if the piece at `from` is a pawn whose move to `to` reaches the promotion rank."
   [from to]
+  (s/assert ::d/square from)
+  (s/assert ::d/square to)
   (rules/pawn-reaching-last-rank? @game-history from to))
